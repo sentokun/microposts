fc76becd44bdf5f228fc7f73272089942ba64c06
sentokun <p53-tb-tashiro@live.jp>
2015-11-05 08:48:01 +0000

    add migrate

--- db/migrate/20150930123134_add_profile_to_user.rb
+++ db/migrate/20150930123134_add_profile_to_user.rb
@@ -1,4 +1,4 @@
-class AddProfilAeToUser < ActiveRecord::Migration
+class AddProfileToUser < ActiveRecord::Migration
   def change
     add_column :users, :profile, :string
     add_column :users, :region, :string
